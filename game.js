e0b90ecc7cfa0d0eb9c241eec97bd4d2ee7b2f01
OscarWoHA <ohalland@gmail.com>
2019-03-04 00:18:10 +0100

    La til litt mer småsnacks

--- game.js
+++ game.js
@@ -27,77 +27,90 @@ let part = 600 / 10
 
 function preload() {
     // Initialize all sounds into sound array
-    sounds.push(
-        { 
-            name: 'intro', 
-            sound: insertAudio('art/soviet_lav.mp3')
-        },
-        {
-            name: 'bullets',
-            sound: loadSound('art/glaz/bullets.mp3'),
-            champion: 1 // glaz
-        },
-        {
-            name: 'oneshot',
-            sound: loadSound('art/glaz/oneshot.mp3'),
-            champion: 1
-        },
-        {
-            name: 'ready',
-            sound: loadSound('art/glaz/ready.mp3'),
-            champion: 1
-        },
-        {
-            name: 'glaz_gun',
-            sound: loadSound('art/glaz/gun.mp3'),
-            champion: 1
-        },
-        {
-            name: 'iknow',
-            sound: loadSound('art/kapkan/iknow.mp3'),
-            champion: 0
-        }, 
-        {
-            name: 'job',
-            sound: loadSound('art/kapkan/job.mp3'),
-            champion: 0
-        }, 
-        {
-            name: 'kapkan_gun',
-            sound: loadSound('art/kapkan/gun.mp3'),
-            champion: 0
-        },
-        {
-            name: 'detected',
-            sound: loadSound('art/tachanka/detected.mp3'),
-            champion: 2
-        }, 
-        {
-            name: 'lmg',
-            sound: loadSound('art/tachanka/lmg.mp3'),
-            champion: 2
-        }, 
-        {
-            name: 'rely',
-            sound: loadSound('art/tachanka/rely.mp3'),
-            champion: 2
-        }, 
-        {
-            name: 'tachanka_gun',
-            sound: loadSound('art/tachanka/gun.mp3'),
-            champion: 2
-        })
+    sounds.push({
+        name: 'intro',
+        sound: insertAudio('art/soviet_lav.mp3')
+    }, {
+        name: 'bullets',
+        sound: loadSound('art/glaz/bullets.mp3'),
+        champion: 1 // glaz
+    }, {
+        name: 'oneshot',
+        sound: loadSound('art/glaz/oneshot.mp3'),
+        champion: 1
+    }, {
+        name: 'ready',
+        sound: loadSound('art/glaz/ready.mp3'),
+        champion: 1
+    }, {
+        name: 'glaz_gun',
+        sound: loadSound('art/glaz/gun.mp3'),
+        champion: 1
+    }, {
+        name: 'iknow',
+        sound: loadSound('art/kapkan/iknow.mp3'),
+        champion: 0
+    }, {
+        name: 'job',
+        sound: loadSound('art/kapkan/job.mp3'),
+        champion: 0
+    }, {
+        name: 'kapkan_gun',
+        sound: loadSound('art/kapkan/gun.mp3'),
+        champion: 0
+    }, {
+        name: 'detected',
+        sound: loadSound('art/tachanka/detected.mp3'),
+        champion: 2
+    }, {
+        name: 'lmg',
+        sound: loadSound('art/tachanka/lmg.mp3'),
+        champion: 2
+    }, {
+        name: 'rely',
+        sound: loadSound('art/tachanka/rely.mp3'),
+        champion: 2
+    }, {
+        name: 'tachanka_gun',
+        sound: loadSound('art/tachanka/gun.mp3'),
+        champion: 2
+    }, {
+        name: 'commie_fucker',
+        sound: loadSound('art/commie_fucker.mp3')
+    })
 
     // Initialize all sprites into sprite array
-    sprites.push(
-        { name: 'glaz', image: loadImage('art/Glaz8bit.png')}, 
-        { name: 'kapkan', image: loadImage('art/Kapkan8bit.png')},
-        { name: 'tachanka', image: loadImage('art/Tachanka8bit.png')},
-        { name: 'background', image: loadImage('art/Spetsnaz8bit.png')},
-        { name: 'spy_left', image: loadImage('art/sprite_0.png')},
-        { name: 'spy_right', image: loadImage('art/sprite_1.png')},
-        { name: 'spy_large', image: loadImage('art/sprite_0_large.png')})
+    sprites.push({
+        name: 'glaz',
+        image: loadImage('art/Glaz8bit.png')
+    }, {
+        name: 'kapkan',
+        image: loadImage('art/Kapkan8bit.png')
+    }, {
+        name: 'tachanka',
+        image: loadImage('art/Tachanka8bit.png')
+    }, {
+        name: 'background',
+        image: loadImage('art/Spetsnaz8bit.png')
+    }, {
+        name: 'spy_left',
+        image: loadImage('art/sprite_0.png')
+    }, {
+        name: 'spy_right',
+        image: loadImage('art/sprite_1.png')
+    }, {
+        name: 'spy_large',
+        image: loadImage('art/sprite_0_large.png')
+    }, {
+        name: 'kremlin',
+        image: loadImage('art/Kremlin.png')
+    }, {
+        name: 'pripyat',
+        image: loadImage('art/Pripyat.png')
+    })
 }
+
+let randomBackground
 
 function setup() {
     createCanvas(600, 600)
@@ -108,7 +121,8 @@ function setup() {
 
     scoreRequired = Math.floor(random(10, 30))
     timeLeft = Math.floor(random(30, 60))
-    
+    randomBackground = Math.floor(random(0, 100))
+
     textFont('VT323')
 }
 
@@ -125,149 +139,149 @@ function draw() {
             textSize(32)
             stroke(100, 0, 0)
             strokeWeight(4)
-            text('SELECT A CHAMPION', width/2, part+32)
+            text('SELECT A CHAMPION', width / 2, part + 32)
             textSize(18)
-            text('USE THE ARROW KEYS, PRESS ENTER WHEN YOU\'RE DONE', width/2, part+32+18)
-    
+            text('USE THE ARROW KEYS, PRESS ENTER WHEN YOU\'RE DONE', width / 2, part + 32 + 18)
+
             let glazSprite = getSprite('glaz')
             let kapkanSprite = getSprite('kapkan')
             let tachankaSprite = getSprite('tachanka')
-    
-            switch(selectedChampion) {
+
+            switch (selectedChampion) {
                 case 0:
                     imageMode(CENTER)
                     textAlign(CENTER)
-    
+
                     fill(255)
                     textSize(24)
-    
-    
+
+
                     // KAPKAN
                     stroke(100, 0, 0)
                     strokeWeight(4)
-                    text('KAPKAN', part*1.5, height/2+(part*1.5+24))
-                    
-                    stroke(255,0,0)
+                    text('KAPKAN', part * 1.5, height / 2 + (part * 1.5 + 24))
+
+                    stroke(255, 0, 0)
                     strokeWeight(1)
-                    line(part*1.5, 0, part*1.5, height)
+                    line(part * 1.5, 0, part * 1.5, height)
                     noStroke()
-    
-                    image(kapkanSprite.image, part*1.5, height/2, part*3, part*3)
-    
+
+                    image(kapkanSprite.image, part * 1.5, height / 2, part * 3, part * 3)
+
                     // GLAZ
                     stroke(100, 0, 0)
                     strokeWeight(4)
-                    text('GLAZ', width/2, height/2+(part*0.75+24))
-    
-                    stroke(255,0,0)
+                    text('GLAZ', width / 2, height / 2 + (part * 0.75 + 24))
+
+                    stroke(255, 0, 0)
                     strokeWeight(1)
-                    line(width/2, 0, width/2, height)
+                    line(width / 2, 0, width / 2, height)
                     noStroke()
-    
-                    image(glazSprite.image, width/2, height/2, part*1.5, part*1.5)
-    
+
+                    image(glazSprite.image, width / 2, height / 2, part * 1.5, part * 1.5)
+
                     // TACHANKA
                     stroke(100, 0, 0)
                     strokeWeight(4)
-                    text('TACHANKA', width-part*1.5, height/2+(part*0.75+24))
-    
-                    stroke(255,0,0)
+                    text('TACHANKA', width - part * 1.5, height / 2 + (part * 0.75 + 24))
+
+                    stroke(255, 0, 0)
                     strokeWeight(1)
-                    line(width-part*1.5, 0, width-part*1.5, height)
+                    line(width - part * 1.5, 0, width - part * 1.5, height)
                     noStroke()
-    
-                    image(tachankaSprite.image, width-part*1.5, height/2, part*1.5, part*1.5)
-    
+
+                    image(tachankaSprite.image, width - part * 1.5, height / 2, part * 1.5, part * 1.5)
+
                     break;
                 case 1:
                     imageMode(CENTER)
                     textAlign(CENTER)
-    
+
                     fill(255)
                     textSize(24)
-    
+
                     // KAPKAN
                     stroke(100, 0, 0)
                     strokeWeight(4)
-                    text('KAPKAN', part*1.5, height/2+(part*0.75+24))   
-                    
-                    stroke(255,0,0)
-                    strokeWeight(1)             
-                    line(part*1.5, 0, part*1.5, height)
+                    text('KAPKAN', part * 1.5, height / 2 + (part * 0.75 + 24))
+
+                    stroke(255, 0, 0)
+                    strokeWeight(1)
+                    line(part * 1.5, 0, part * 1.5, height)
                     noStroke()
-    
-                    image(kapkanSprite.image, part*1.5, height/2, part*1.5, part*1.5)
-    
-                    
+
+                    image(kapkanSprite.image, part * 1.5, height / 2, part * 1.5, part * 1.5)
+
+
                     // GLAZ
                     stroke(100, 0, 0)
                     strokeWeight(4)
-                    text('GLAZ', width/2, height/2+(part*1.5+24))
-    
-                    stroke(255,0,0) 
-                    strokeWeight(1)            
-                    line(width/2, 0, width/2, height)
+                    text('GLAZ', width / 2, height / 2 + (part * 1.5 + 24))
+
+                    stroke(255, 0, 0)
+                    strokeWeight(1)
+                    line(width / 2, 0, width / 2, height)
                     noStroke()
-    
-                    image(glazSprite.image, width/2, height/2, part*3, part*3)
-    
+
+                    image(glazSprite.image, width / 2, height / 2, part * 3, part * 3)
+
                     // TACHANKA
                     stroke(100, 0, 0)
                     strokeWeight(4)
-                    text('TACHANKA', width-part*1.5, height/2+(part*0.75+24))
-    
+                    text('TACHANKA', width - part * 1.5, height / 2 + (part * 0.75 + 24))
+
                     stroke(255, 0, 0)
                     strokeWeight(1)
-                    line(width-part*1.5, 0, width-part*1.5, height)
+                    line(width - part * 1.5, 0, width - part * 1.5, height)
                     noStroke()
-                    
-                    image(tachankaSprite.image, width-part*1.5, height/2, part*1.5, part*1.5)
-    
+
+                    image(tachankaSprite.image, width - part * 1.5, height / 2, part * 1.5, part * 1.5)
+
                     break;
                 case 2:
                     imageMode(CENTER)
                     textAlign(CENTER)
-    
+
                     fill(255)
                     textSize(24)
-    
-    
+
+
                     // KAPKAN
                     stroke(100, 0, 0)
                     strokeWeight(4)
-                    text('KAPKAN', part*1.5, height/2+(part*0.75+24))
-                    
-                    stroke(255,0,0)
+                    text('KAPKAN', part * 1.5, height / 2 + (part * 0.75 + 24))
+
+                    stroke(255, 0, 0)
                     strokeWeight(1)
-                    line(part*1.5, 0, part*1.5, height)
+                    line(part * 1.5, 0, part * 1.5, height)
                     noStroke()
-    
-                    image(kapkanSprite.image, part*1.5, height/2, part*1.5, part*1.5)
-    
+
+                    image(kapkanSprite.image, part * 1.5, height / 2, part * 1.5, part * 1.5)
+
                     // GLAZ
                     stroke(100, 0, 0)
                     strokeWeight(4)
-                    text('GLAZ', width/2, height/2+(part*0.75+24))
-                    
-                    stroke(255,0,0)
+                    text('GLAZ', width / 2, height / 2 + (part * 0.75 + 24))
+
+                    stroke(255, 0, 0)
                     strokeWeight(1)
-                    line(width/2, 0, width/2, height)
+                    line(width / 2, 0, width / 2, height)
                     noStroke()
-    
-                    image(glazSprite.image, width/2, height/2, part*1.5, part*1.5)
-    
+
+                    image(glazSprite.image, width / 2, height / 2, part * 1.5, part * 1.5)
+
                     // TACHANKA
                     stroke(100, 0, 0)
                     strokeWeight(4)
-                    text('TACHANKA', width-part*1.5, height/2+(part*1.5+24))
-    
-                    stroke(255,0,0)
+                    text('TACHANKA', width - part * 1.5, height / 2 + (part * 1.5 + 24))
+
+                    stroke(255, 0, 0)
                     strokeWeight(1)
-                    line(width-part*1.5, 0, width-part*1.5, height)
+                    line(width - part * 1.5, 0, width - part * 1.5, height)
                     noStroke()
-    
-                    image(tachankaSprite.image, width-part*1.5, height/2, part*3, part*3)
-    
+
+                    image(tachankaSprite.image, width - part * 1.5, height / 2, part * 3, part * 3)
+
                     break;
                 default:
                     break;
@@ -299,16 +313,22 @@ function draw() {
             fill(255)
             textSize(16)
             textAlign(CENTER)
-            text('WANTED', width - size/2 - 5, size + 20)
+            text('WANTED', width - size / 2 - 5, size + 20)
         }, () => {
             background(0)
             fill(255, 0, 0)
             textAlign(CENTER)
             textSize(24)
-            text(`PLEASE INTERACT WITH THE PAGE`, width/2, height/2)
+            text(`PLEASE INTERACT WITH THE PAGE`, width / 2, height / 2)
         })
     } else if (gameState == 1) { // Kjører
         background(0) // Set the background to black every frame
+
+        if (randomBackground > 50) {
+            image(getSprite('kremlin').image, 0, 0, width, height)
+        } else {
+            image(getSprite('pripyat').image, 0, 0, width, height)
+        }
 
         getSound('intro').sound.pause() // Stop intro
 
@@ -323,16 +343,16 @@ function draw() {
         strokeWeight(5)
 
         if (timeLeft > 0) {
-            text('You won!', width/2, height/2)
+            text('You won!', width / 2, height / 2)
 
             textSize(32)
 
             let textOffset = 32
 
-            text(`Final time`, width / 2, height / 2 + textOffset*2)
+            text(`Final time`, width / 2, height / 2 + textOffset * 2)
 
             textSize(24)
-            text(`${timeLeft} seconds`, width / 2, height / 2 + textOffset*2.75)
+            text(`${timeLeft} seconds`, width / 2, height / 2 + textOffset * 2.75)
         } else {
             text('Times up!', width / 2, height / 2)
         }
@@ -356,7 +376,7 @@ function drawGame() {
 
     textSize(16)
     textAlign(RIGHT)
-    
+
     text(scoreText, textWidth(scoreText) + 6, 18)
     text(scoreRequiredText, textWidth(scoreRequiredText) + 6, 36)
     text(timeLeftText, textWidth(timeLeftText) + 6, 54)
@@ -371,14 +391,14 @@ function keyPressed() {
             let sounds = getSoundsByChampion(selectedChampion)
 
             if (sounds.length != 0) {
-                sounds[Math.floor(random(0, sounds.length-1))].sound.play()
+                sounds[Math.floor(random(0, sounds.length - 1))].sound.play()
             }
         } else if (keyCode == RIGHT_ARROW) {
             if (selectedChampion == 2) selectedChampion = 0
             else selectedChampion += 1
 
             let sounds = getSoundsByChampion(selectedChampion)
-            
+
             if (sounds.length != 0) {
                 sounds[Math.floor(random(0, sounds.length - 1))].sound.play()
             }
@@ -400,7 +420,7 @@ function keyPressed() {
             if (volume < 0) volume = 0
 
             sounds.forEach(sound => {
-                sound.sound.volume = volume/10
+                sound.sound.volume = volume / 10
             })
 
             masterVolume(volume / 10)
@@ -410,7 +430,7 @@ function keyPressed() {
             if (volume > 10) volume = 10
 
             sounds.forEach(sound => {
-                sound.sound.volume = volume/10
+                sound.sound.volume = volume / 10
             })
 
             masterVolume(volume / 10)
@@ -427,6 +447,11 @@ function mouseClicked() {
         if (!sound.sound.isPlaying())
             sound.sound.play()
 
+        sound.sound.onended(() => {
+            if (!getSound('commie_fucker').sound.isPlaying())
+                getSound('commie_fucker').sound.play()
+        })
+
         if (mouseX >= mob.x && mouseX <= (mob.x + part)) {
             if (mouseY >= mob.y && mouseY <= (mob.y + part)) {
                 mob.x = random(0, width - part)
@@ -435,7 +460,7 @@ function mouseClicked() {
                 mob.speed_x = Math.floor(random(2, 6))
                 mob.speed_y = Math.floor(random(2, 6))
 
-                
+
                 score += 1
 
                 if (score == scoreRequired) {
@@ -453,7 +478,9 @@ function mouseClicked() {
  * @param {string} name 
  */
 function getSprite(name) {
-    return sprites.filter(sprite => { return sprite.name == name })[0]
+    return sprites.filter(sprite => {
+        return sprite.name == name
+    })[0]
 }
 
 /**
@@ -461,11 +488,15 @@ function getSprite(name) {
  * @param {string} name 
  */
 function getSound(name) {
-    return sounds.filter(sound => { return sound.name == name})[0]
+    return sounds.filter(sound => {
+        return sound.name == name
+    })[0]
 }
 
 function getSoundsByChampion(champion) {
-    return sounds.filter(sound => { return sound.champion == champion && !sound.name.includes('_gun') })
+    return sounds.filter(sound => {
+        return sound.champion == champion && !sound.name.includes('_gun')
+    })
 }
 
 /**
@@ -492,7 +523,7 @@ function insertAudio(file, options) {
             songelement.controls = ''
         }
     }
-    
+
     songelement.volume = volume / 10
 
     // Append the virtual HTML element to the actual DOM
